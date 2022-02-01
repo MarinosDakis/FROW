de2a805f5862b14d8dfb638bc19ed8e0493a77f6
Dakis <marinos.dakis@capgemini.com>
2022-02-01 13:40:06 -0500

    edit Navbar.js to update Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,5 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+import { AppBar, Typography, Toolbar, Avatar, Button, Box } from '@material-ui/core'
+import { Link } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
+import useStyles from "./styles";
+import FrowLogo from "../../images/frow_logo.png";
 
-export default function Navbar() {
-  return <div>This is Navbar</div>;
-}
+const Navbar = () => {
+  const classes = useStyles();
+
+  /*
+  //default user is information we stored in local storage from google sign in
+  const [user, setUser] = true;
+  const dispatch = useDispatch();
+
+  const logout = () => {
+      dispatch({ type: "LOGOUT" })
+
+      // after user logs out we push them to the home route
+      navigate.push("/");
+
+      // set user to null after
+      setUser(null);
+  }
+ 
+  useEffect(() => {
+      const token = user?.token;
+
+      // check if JWT token has expired
+
+      // see if token exists
+          if (token) {
+              const decodedToken = decode(token);
+
+           // if expired log user out
+              if (decodedToken.exp * 1000 < new Date().getTime()){
+                  logout();
+              }
+          }
+          
+      setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location]);
+*/
+
+  //       <img src={FrowLogo} alt="logo" className={classes.image}></img>
+
+  return (
+    <AppBar className={classes.appBar}>
+      <Box className={classes.brandContainer}>
+        <Typography className={classes.heading} component={Link} to="/" variant="h2" align="center">FROW</Typography>
+        <img src={FrowLogo} alt="logo" className={classes.image} height="60"></img>
+      </Box>
+      <Box>
+        <Button component={Link} to="/login" variant="contained" className={classes.button}>Sign In</Button>
+      </Box>
+    </AppBar>
+  );
+};
+
+export default Navbar
---
+++ client/src/components/Navbar/styles.js
@@ -0,0 +1,47 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+    button: {
+        "&.MuiButton-contained": {
+            color: "white",
+            backgroundColor: "black",
+            height: 40,
+            width: 100,
+        },
+    },
+    appBar: {
+        borderRadius: 5,
+        margin: '30px 0',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px 50px',
+        backgroundColor: 'white',
+    },
+    heading: {
+        color: "black",
+        textDecoration: 'none',
+    },
+    image: {
+        marginLeft: 30,
+    },
+    toolbar: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        width: '400px',
+    },
+    profile: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '400px',
+    },
+    userName: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+    brandContainer: {
+        display: 'flex',
+        alignItems: 'center',
+    },
+}));
