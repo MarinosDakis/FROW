b0d2e7466db56987170310cbe4575afdcac06455
Bryant Hunsberger <Bryant.Hunsberger@gmail.com>
2022-01-31 20:18:29 -0500

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Welcome from '../src/pages/Home.js';
 
-function App() {
-  return (
-    // This is a browser router, a browser router allows us to dynamically add paths
-      <div className="App">
-          <BrowserRouter>
-              <div>
-                <Route exact path='/' component={Welcome} />
-              </div>
-          </BrowserRouter>
-      </div>
-  );
-}
-
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>, 
+  document.getElementById('root')
+);
