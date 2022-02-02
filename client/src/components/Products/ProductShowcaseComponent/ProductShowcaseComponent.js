58dd4097794f41532582a078d4f10d99423db639
Dakis <marinos.dakis@capgemini.com>
2022-02-02 17:23:44 -0500

    refactor code for Products.js and edit css

--- client/src/components/Products/ProductShowcaseComponent/ProductShowcaseComponent.js
+++ client/src/components/Products/ProductShowcaseComponent/ProductShowcaseComponent.js
@@ -1,5 +1,64 @@
-import React from 'react';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-export default function ProductShowcaseComponent() {
-  return <div></div>;
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function ProductShowcaseComponent(props) {
+  const { productName, productDescription, productPrice, productImg } = props;
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        title={productName}
+        subheader={`$${productPrice}`}
+      />
+      <CardMedia
+        component="img"
+        height="250"
+        width="250"
+        image={productImg}
+        alt={`${productName}-img`}
+      />
+
+      <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
+
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>{productDescription}</Typography>
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
--- client/src/components/Products/Products.js
+++ client/src/components/Products/Products.js
@@ -3,47 +3,30 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import Data from './data.json';
 import dress from './images/dress.png';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import { Button, Grid } from '@material-ui/core';
 import CreditCardIcon from '@material-ui/icons/CreditCard';
 import EmailIcon from '@material-ui/icons/Email';
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { ExpandMore } from '@material-ui/icons';
+import ProductShowcaseComponent from './ProductShowcaseComponent/ProductShowcaseComponent';
 
 function Products() {
-
-  const [expandedPanel, setExpandedPanel] = useState(false);
-
-  const handleAccordionChange = (panel) => (event, isExpanded) => {
-    console.log({ event, isExpanded });
-    setExpandedPanel(isExpanded ? panel : false);
-  };
 
   return (
     <div className='ProductsContainer' style={{ paddingTop: '110px' }}>
       <div className='TopBanner'>
         <Grid container spacing={2}
-          direction="row"
           alignItems="center"
           justifyContent="center">
 
-          <Grid item xs="auto" direction="row">
+          <Grid item xs="auto">
             <Button variant="contained" startIcon={<EmailIcon />}> Email Designer</Button>
           </Grid>
-          <Grid item xs="auto" direction="row">
+          <Grid item xs="auto">
             <Button variant="contained" startIcon={<CreditCardIcon />}> Purchase Line</Button>
           </Grid>
         </Grid>
 
         <Grid container spacing={2}
-          direction="row"
           alignItems="center"
           justifyContent="center">
           <Grid item xs="auto" >
@@ -53,7 +36,6 @@ function Products() {
         </Grid>
 
         <Grid container spacing={2}
-          direction="row"
           alignItems="center"
           justifyContent="center">
 
@@ -63,43 +45,14 @@ function Products() {
         </Grid>
       </div>
 
-      <Grid container spacing={3}>
-        {Data.map(product => (
-          <Grid item key={product.id} xs={3} >
-            <Card sx={{ maxWidth: 345 }} >
-              <CardMedia
-                component="img"
-                height="100%"
-                image={dress}
-                alt="dress"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="subtitle1" component="div" style={{ fontWeight: "bold" }}>
-                  {product.title}
-                </Typography>
-
-                <Accordion expanded={expandedPanel === 'panel1'} onChange={handleAccordionChange('panel1')}>
-
-                  <AccordionSummary expandIcon={<ExpandMore />}>
-                    Description
-                  </AccordionSummary>
-
-                  <AccordionDetails>
-                  {product.desc}
-                  </AccordionDetails>
-
-                </Accordion>
-
-
-
-
-
-
-              </CardContent>
-              <CardActions>
-                <Button style={{ fontWeight: "bold" }} size="small">${product.price}</Button>
-              </CardActions>
-            </Card>
+      <Grid container spacing={4}>
+        {Data.map((product) => (
+          <Grid item key={product.id} xs={4} >
+            <ProductShowcaseComponent
+              productName={product.title}
+              productDescription={product.desc}
+              productPrice={product.price}
+              productImg={dress} />
           </Grid>
         ))}
       </Grid>
