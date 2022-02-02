24bb1182b8f4b340c612681062fcf76aa2a01166
jrooney99 <98781632+jrooney99@users.noreply.github.com>
2022-02-02 12:03:30 -0600

    fix 2 payment page

---
+++ client/src/components/Payments/format.js
@@ -0,0 +1,46 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+
+header:{
+    fontSize: 40,
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 40,
+},
+root: {
+    marginTop: 100,
+    padding: theme.spacing(1),
+},
+paper: {
+    backgroundColor: "white",
+    margin: "20px 0",
+    borderRadius: 15,
+    border: "#7e7878 solid 1px",
+    boxShadow: "0 4px 5px 5px rgba(0, 0, 0, .2)",
+    height: 500,
+},
+grid: {
+    display: "Flex",
+    justifyContent: "Center"
+},
+grid2:{
+    display: "Flex", 
+    justifyContent: "Denter"
+},
+Paragraph:{
+    fontSize: 35,
+    display: "flex",
+    justifyContent: "center",
+    marginTop: 30,
+},
+})
+
+);
+
+
+/*
+<Typography variant="Purchase Successful!" align="right">
+h3 align right
+</Typography>
+*/
